713797aa97f1f3c33428875dbdb4616d6103f48d
Julianna Leila Castillo <119785135+nintendocicc@users.noreply.github.com>
2025-05-17 13:35:13 +0800

    Updated naming convention

--- Original/BubbleSort.cpp
+++ Original/BubbleSort.cpp
@@ -1,44 +1,44 @@
 #include <iostream>
 using namespace std;
 
-int bubbleSort(int array[], int size){
-	int temp = 0;
-	int passCounter = 0;
+int bubbleSort(int intArray[], int intSize){
+	int intTemp = 0;
+	int intPassCounter = 0;
 	
-	for(int i = 0; i < size - 1; i++){
-		passCounter++; // Increments for each pass done by the algorithm
-		for(int j = 0; j < size - i - 1; j++){
-			if(array[j] > array[j + 1]){
-				temp = array[j];
-				array[j] = array[j + 1];
-				array[j + 1] = temp;
+	for(int i = 0; i < intSize - 1; i++){
+		intPassCounter++; // Increments for each pass done by the algorithm
+		for(int j = 0; j < intSize - i - 1; j++){
+			if(intArray[j] > intArray[j + 1]){
+				intTemp = intArray[j];
+				intArray[j] = intArray[j + 1];
+				intArray[j + 1] = intTemp;
 			}
 		}
-	} return passCounter;
+	} return intPassCounter;
 }
 
-void printArray(int array[], int size){
-    for(int i = 0; i < size; i++){
-        cout << array[i] << " ";
+void printArray(int intArray[], int intSize){
+    for(int i = 0; i < intSize; i++){
+        cout << intArray[i] << " ";
     }
     cout << endl;
 }
 
 int main(){
-    int array[] = {63, 48, 73, 24, 17, 19};
-    int size = sizeof(array) / sizeof(array[0]);
-    int passes = bubbleSort(array, size);
+    int intArray[] = {63, 48, 73, 24, 17, 19};
+    int intSize = sizeof(intArray) / sizeof(intArray[0]);
+    int intPasses = bubbleSort(intArray, intSize);
 
 	// Prints unsorted array
     cout << "Unsorted Array: ";
-    printArray(array, size);
+    printArray(intArray, intSize);
 
 	// Prints sorted array
     cout << "Sorted Array: ";
-    printArray(array, size);
+    printArray(intArray, intSize);
 
 	// Prints number of passes
-    cout << "Number of Passes: " << passes << endl;
+    cout << "Number of Passes: " << intPasses << endl;
 
     return 0;
 }
