ca7c6b81c98aab130c81034dbfccb4952edb2e70
Xuinnz <redtagura07@gmail.com>
2025-05-17 14:51:48 +0800

    Update KnapsackProblem.cpp
    no user input

--- Original/KnapsackProblem.cpp
+++ Original/KnapsackProblem.cpp
@@ -2,28 +2,15 @@
 using namespace std;
 
 int main() {
-
-    int n, maxWeight, sizeCounter = 0;
-    cout << "Enter size: ";
-    cin >> n;
-    cout <<"\nEnter max weight: ";
-    cin >> maxWeight;
-    int item[n][2];
-    for (sizeCounter = 0; sizeCounter < n; sizeCounter++){
-        cout << "Enter weight of Item " << sizeCounter + 1 << ": ";
-        cin >> item[sizeCounter][0];
-        cout << "\nEnter Value of Item " << sizeCounter + 1 << ": ";
-        cin >> item[sizeCounter][1];
-    }
+    int item[4][2] = {{1, 1}, {2, 6}, {3, 10}, {2, 7}};
+    int n = 4;
     int mask, i;
     int totalWeight, totalValue;
-    int highestValue = 0;
+    int maxWeight = 5, highestValue = 0;
     for(mask = 0; mask < (1 << n); mask++){ 
         totalWeight = 0; 
         totalValue = 0; 
-        
-        cout << endl; 
-        cout << "Subset: "; 
+        cout << "\nSubset: "; 
         for(i = 0; i < n; i++){ 
             if(mask & (1 << i)){ 
                 totalWeight += item[i][0]; 
@@ -31,7 +18,7 @@ int main() {
                 cout << i + 1 << " "; 
             } 
         }
-        if(totalWeight <= maxWeight){ 
+        if(totalWeight <= maxWeight){
             if(highestValue < totalValue){ 
                 highestValue = totalValue; 
             }
@@ -41,7 +28,8 @@ int main() {
         }
         cout << endl << "Total Weight: " << totalWeight << endl << "Total Value: " << totalValue << endl; 
     }
-    cout << "Highest value with total with " << maxWeight << "kg restriction: " << highestValue; 
+    cout << "Highest value with total with 5kg restriction: " << highestValue; 
 
+ 
     return 0;
 }
