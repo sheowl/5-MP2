f9b6e0fb578c126c418b74988432d67e7edd5a9e
FvlSky <mariahannavb@gmail.com>
2025-05-17 16:35:46 +0800

    Finalized selection sort for both original and optimized (bidirectional)

--- Original/SelectionSort.cpp
+++ Original/SelectionSort.cpp
@@ -2,40 +2,45 @@
 using namespace std;
 
 //Original and unoptimized code for selection sort
-int selectionSort(int array[], int arrLength){
+int selectionSort(int arrNumbers[], int intArrLength){
     int intPassCount = 0;
-    for(int i = 0; i < (arrLength - 1); i++){
+
+    //Iterates through the array and selects the minimum value in the unsorted portion
+    for(int i = 0; i < (intArrLength - 1); i++){ 
         int min = i;
-        for(int j = i + 1; j < arrLength; j++){
-            if(array[j] < array[min]){
-                min = j;
+        for(int j = i + 1; j < intArrLength; j++){
+            if(arrNumbers[j] < arrNumbers[min]){
+                min = j; 
             }
         }
 
+        //Swaps the current element with the found minimum value
         if(min != i){
-            int temp = array[i];
-            array[i] = array[min];
-            array[min] = temp;
+            int temp = arrNumbers[i];
+            arrNumbers[i] = arrNumbers[min];
+            arrNumbers[min] = temp;
         }
-        
+
         intPassCount++;
     }
     return intPassCount;
 }
 
-void printArray(int array[], int arrLength){
-    for(int i = 0; i < arrLength; i++){
-        cout << array[i] << " ";
+//Printing arrNumbers function
+void printArray(int arrNumbers[], int intArrLength){
+    for(int i = 0; i < intArrLength; i++){
+        cout << arrNumbers[i] << " ";
     }
 }
 
+//main function
 int main(){
-    int array[] = {25, 10, 2, 5, 12, 45, 60, 32, 56, 72, 88, 92};
-    int arrLength = sizeof(array) / sizeof(array[0]);
+    int arrNumbers[] = {19, 42, 12, 71, 201, 9, 14, 56, 2, 2941, 0, 19};
+    int intArrLength = sizeof(arrNumbers) / sizeof(arrNumbers[0]);
 
-    cout << "Unsorted Array: "; printArray(array, arrLength);
-    int intPasses = selectionSort(array, arrLength);
-    cout << "\nSorted Array: "; printArray(array, arrLength);
+    cout << "Unsorted Array: "; printArray(arrNumbers, intArrLength);
+    int intPasses = selectionSort(arrNumbers, intArrLength);
+    cout << "\nSorted Array: "; printArray(arrNumbers, intArrLength);
     cout << "\nNumber of Passes: " << intPasses;
 
     return 0;
